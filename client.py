2fbd3368eae1357c063713efb3c80e9b99833396
Nizarras <65706624+Nizarras@users.noreply.github.com>
2022-02-05 19:53:34 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,14 +1,14 @@
 #coding utf-8
 import socket
 
-host, port = ('127.0.0.1', 5566)   #'127.0.0.1' = local host pour toutes eles machine, 5566) 
+host, port = ('127.0.0.1', 5566)   #'127.0.0.1' = local host pour toutes eles machine, 5566 pour le port ouvert) 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     socket.connect((host,port))
     print ("client connecté") #partie a modifier en fonc de l uti
     
-    data = "Bonjour je suis le client " # A passer en input indiquer le nom de l'uti :
+    data = "Bonjour je suis le client " # A passer en input indiquer le nom de l'uti reseigner l uti plutot le stoker dans une variable et l indiquer __>>
     data = data.encode("utf8")
     socket.sendall(data)
     
